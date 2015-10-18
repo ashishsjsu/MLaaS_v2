f806c0ea05db12dd44c73b1c84a8c126e88dc164
ashishsjsu <ashish12021@gmail.com>
2015-10-17 23:41:25 -0700

    API for signing the file before uploading to S3

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, jsonify, url_for
-import time
+from flask import Blueprint, jsonify, url_for, request
+import time, os, urllib, base64, hmac
 from datetime import datetime
 from elasticsearch import Elasticsearch
 
@@ -18,6 +18,41 @@ def sayHello():
 		return jsonify({"message": "Hello!"})
 	except:
 		abort(404)
+
+#this api is meant to return the signature with which client can upload the file to AWS S3
+@sparktask_page.route('/upload/sign_s3', methods=["GET"])
+def upload_file():
+
+	# get AWS parameters needed to sign the request
+	AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')
+	AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
+	S3_BUCKET = os.environ.get('S3_BUCKET')
+
+	#object name to be uploaded, get file name from the req url params
+	object_name = urllib.quote_plus(request.args.get('file_name'))
+	# get the type of file
+	mime_type = request.args.get('file_type')
+
+	# set expiry time of the signature to one day, this is a temporary request
+	expires = int(time.time()+60*60*24)
+	# this header indicates that the file is publically available for download
+	amz_headers = "x-amz-acl:public-read"
+	# construct the PUT request sing above data
+	string_to_sign = "PUT\n\n%s\n%d\n%s\n/%s/%s" % (mime_type, expires, amz_headers, S3_BUCKET, object_name)
+	# generate the signature as SHA hash of compiled AWS swcret key and PUT request
+	signature = base64.encodestring(hmac.new(AWS_SECRET_KEY.encode(), string_to_sign.encode('utf8'), sha1).digest())
+	# strip the whitespaces and escape the special characters for safer HTTP transmission
+	signature = urllib.quote_plus(signature.strip())
+	# url of the object to be uploaded
+	url = "https://%s.s3.amazonaws.com/%s" % (S3_BUCKET, object_name)
+	# return the signed request and the prospective url as json response
+	response = json.dumps({
+		'signed_request': "%s?AWSAccessKeyId=%s&Expires=%s&Signature=%s" % (url, AWS_ACCESS_KEY, AWS_SECRET_KEY, expires, signature),
+		'url': url
+	})
+
+	return response
+
 
 #route to submit a spark job
 @sparktask_page.route('/spark_task', methods=["POST"])
@@ -70,3 +105,4 @@ def taskstatus(task_id):
 		response['state'] = task.state
 
 	return jsonify(response)
+
